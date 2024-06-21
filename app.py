9066838d4c60e8723a1cf617a432de4bbbd02ee9
Vishnudivakaran17 <159907310+Vishnudivakaran17@users.noreply.github.com>
2024-06-21 14:48:10 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="My webpage", page_icon=":tada:",layout="wide")
+
+
+st.subheader("hi, i am vishnu :wave:")
+st.title ("Mechanical Engineer from India")
